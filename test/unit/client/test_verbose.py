27d4827dc590890fbb71e828bc0ae77bde8dc3f7
Max Lapan <maxim.lapan@exasol.com>
2026-09-17 10:43:51 +0200

    Support of v0.2 protocol (#5)
    * Protocol specification
    
    * Initial refactor - sending is broken
    
    * Worker refactor
    
    * Worker functionality
    
    * Call setup() on track()
    
    * Tests
    
    * More tests
    
    * Fixes
    
    * Typo
    
    * Stupid github
    
    * Format
    
    * Fix doc
    
    * Typing
    
    * Fix
    
    * Fix
    
    * py.typed
    
    * Documentation
    
    * Typo
    
    * Verbose mode
    
    * Move typed file
    
    * Release preparation
    
    * Format fix
    
    * Update doc/protocol.rst
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>
    
    * Update doc/protocol.rst
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>
    
    * Update doc/protocol.rst
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>
    
    * On disable() call shut down the worker
    
    * Format
    
    * Lock on deferred setup
    
    * Fix doc
    
    * Update integration test
    
    * Update release version
    
    ---------
    
    Co-authored-by: Torsten Kilias <tkilias@users.noreply.github.com>

---
+++ test/unit/client/test_verbose.py
@@ -0,0 +1,24 @@
+import logging
+
+from exasol.telemetry.client import verbose
+
+
+def test_no_show_unconfigured(caplog, telemetry_reset):
+    logging.log(verbose.LEVEL, "Test")
+    assert "Test" not in caplog.text
+    caplog.clear()
+
+    verbose.log("Test")
+    assert "Test" not in caplog.text
+
+
+def test_show_configured(caplog, telemetry_reset):
+    verbose.setup_logging()
+    verbose.log("Test")
+    assert "Test" in caplog.text
+
+    # second setup changes nothing
+    verbose.setup_logging()
+    caplog.clear()
+    verbose.log("Test2")
+    assert "Test2" in caplog.text
